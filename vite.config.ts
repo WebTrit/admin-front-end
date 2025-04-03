be9b73df2becd3688658272614d84042683ebe1f
Andrii Yakovliev <a.yakovliev@webtrit.com>
2025-04-03 15:07:00 +0300

    feat: add docker support

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-  },
+    base: "/",
+    build: {
+        outDir: "dist",
+    },
+    server: {
+        port: 3000,
+        host: "0.0.0.0",
+    },
+    preview: {
+        port: 8080,
+        host: "0.0.0.0",
+    },
 });
