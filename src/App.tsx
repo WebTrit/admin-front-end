afe4e58c4f96d59389d3b7194e7bf9469dfa8751
Andrii Yakovliev <a.yakovliev@webtrit.com>
2025-04-16 12:47:33 +0300

    fix: remove logs

--- src/App.tsx
+++ src/App.tsx
@@ -25,21 +25,6 @@ function App() {
     // Check authentication on app load
     useEffect(() => {
         checkAuth()
-        console.log('VITE Environment Variables:');
-        console.log('VITE_BACKEND_URL:', import.meta.env.VITE_BACKEND_URL);
-        console.log('VITE_WEBTRIT_URL:', import.meta.env.VITE_WEBTRIT_URL);
-        console.log('VITE_WEBTRIT_GOOGLE_PLAY_URL:', import.meta.env.VITE_WEBTRIT_GOOGLE_PLAY_URL);
-        console.log('VITE_WEBTRIT_APP_STORE_URL:', import.meta.env.VITE_WEBTRIT_APP_STORE_URL);
-
-        console.log('VITE_IS_SIGNUP_COMPANY_SITE:', import.meta.env.VITE_IS_SIGNUP_COMPANY_SITE);
-        console.log('VITE_IS_SIGNUP_COMPANY_NAME:', import.meta.env.VITE_IS_SIGNUP_COMPANY_NAME);
-        console.log('VITE_IS_SIGNUP_PHONE_NUMBER:', import.meta.env.VITE_IS_SIGNUP_PHONE_NUMBER);
-
-        console.log('VITE_APP_TITLE:', import.meta.env.VITE_APP_TITLE);
-        console.log('VITE_APP_IS_DASHBOARD_INVITE:', import.meta.env.VITE_APP_IS_DASHBOARD_INVITE);
-        console.log('VITE_APP_IS_DASHBOARD_CONNECT_PBX:', import.meta.env.VITE_APP_IS_DASHBOARD_CONNECT_PBX);
-        console.log('VITE_APP_IS_DASHBOARD_DEVELOPER_ACCESS:', import.meta.env.VITE_APP_IS_DASHBOARD_DEVELOPER_ACCESS);
-
     }, [checkAuth])
 
     return (
