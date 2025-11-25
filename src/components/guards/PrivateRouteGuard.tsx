dd370d0a22cc73ffbaa58daf17218d3b53f4c905
Andrii Yakovliev <a.yakovliev@webtrit.com>
2025-11-25 12:44:16 +0200

    my.webtrit.com should open the login page by default

--- src/components/guards/PrivateRouteGuard.tsx
+++ src/components/guards/PrivateRouteGuard.tsx
@@ -1,14 +1,10 @@
 import {Navigate} from 'react-router-dom';
 import {useAppStore} from "@/lib/store.ts";
-import {config} from "@/config/runtime";
 
 function PrivateRouteGuard({children}: { children: React.ReactNode }) {
     const isAuthenticated = useAppStore((state) => state.isAuthenticated)
-    const VITE_IS_SIGNUP = config.IS_SIGNUP
 
-    if (!isAuthenticated && VITE_IS_SIGNUP) {
-        return <Navigate to="/signup" replace/>
-    } else if (!isAuthenticated && !VITE_IS_SIGNUP) {
+    if (!isAuthenticated) {
         return <Navigate to="/login" replace/>
     }
 
