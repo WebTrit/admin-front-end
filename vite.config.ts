9871e08e2fcd71df7f48ad9c985c96e88d95d17b
Andrii Yakovliev <a.yakovliev@webtrit.com>
2026-01-16 17:12:38 +0200

    fix: Add versioning for tenant portal (#20)

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {createHtmlPlugin} from 'vite-plugin-html';
+import packageJson from './package.json';
 
 export default defineConfig(({mode}) => {
     const env = loadEnv(mode, process.cwd(), '');
 
     return {
+        define: {
+            __APP_VERSION__: JSON.stringify(packageJson.version),
+        },
         plugins: [
             react(),
             createHtmlPlugin({
